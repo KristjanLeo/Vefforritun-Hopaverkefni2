5cd5247dc197e8c56058d822b9d93578ba9334b9
KristjanLeo <krissi.leo@gmail.com>
2020-11-19 19:10:23 +0000

    Correct video loads

--- src/videos.js
+++ src/videos.js
@@ -2,21 +2,26 @@ import { element, videoLength, videoAge } from './lib/utils';
 import fetchVideos from './lib/fetchvideos';
 
 
-async function displayvideo(videodata){
+async function readVideoID(videodata){
   try {
     const url_string = (window.location.href).toLowerCase();
     const url = new URL(url_string);
     const videoId = url.searchParams.get('id');
-    console.log(videoId);
+    displayvideo(videodata, videoId);
   } catch (err) {
     console.log('Error' + err);
   }
 }
 
+async function displayvideo(videos, theid){
+  const BODY = document.querySelector('body');
+  const Header = element('h1', {}, {}, videos.videos[theid-1].title);
+  BODY.appendChild(Header);
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
 
   await fetchVideos().then((data) => {
-    displayvideo(data);
+    readVideoID(data);
   });
 });
