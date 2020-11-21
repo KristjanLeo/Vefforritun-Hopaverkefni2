3bf76f66b17392841ed82f63d9cb4ed6f834236e
valdi7913 <valdi7913@gmail.com>
2020-11-21 12:53:14 +0000

    bætti við lengd myndbandanna á thumbnailið

--- src/index.js
+++ src/index.js
@@ -27,31 +27,36 @@ const Thumbnails = new Array(Categories.length);
 const Info = new Array(Categories.length);
 const Title = new Array(Categories.length);
 const Time = new Array(Categories.length);
-
+const Length = new Array(Categories.length);
+const Div = new Array(Categories.length);
 for(let i = 0; i < Categories.length; i++){
   Header[i] = element('h2', {'class' : 'col col-12'}, {}, Categories[i].title);
   Headers[i] = element('div', {'class' : 'row'}, {}, Header[i]);
   VideoRows[i] = element('div', {'class' : 'row'}, {}, '');
   Videos[i] = new Array(Categories[i].videos.length);
+  Div[i] = new Array(Categories[i].videos.length);
   Thumbnails[i] = new Array(Categories[i].videos.length);
   Info[i] = new Array(Categories[i].videos.length);
   Title[i] = new Array(Categories[i].videos.length);
   Time[i] = new Array(Categories[i].videos.length);
-
+  Length[i] = new Array(Categories[i].videos.length);
   for(let j = 0; j < Videos[i].length; j++){
     const videoId = Categories[i].videos[j];
     Thumbnails[i][j] = element('img', {'src' : videos.videos[videoId - 1].poster, 'class' : 'thumbnails'}, {}, '');
     Title[i][j] = element('h4', {}, {}, videos.videos[videoId - 1].title);
     Time[i][j] = element('h5', {}, {}, videoAge(videos.videos[videoId - 1].created));
+    Length[i][j] = element('span', {'class': 'display-time'}, {}, videoLength(videos.videos[videoId - 1].duration));
+    Div[i][j] =  element('div', {'class': 'thumbnail-container'}, {}, Thumbnails[i][j],Length[i][j])
     Info[i][j] = element('div', {'class' : 'info'}, {}, Title[i][j], Time[i][j]);
-    Videos[i][j] = element('div', {'class' : 'col col-4 col-sm-10 offset-col-sm-1 videocontainer'}, {}, Thumbnails[i][j], Info[i][j]);
+    Videos[i][j] = element('div', {'class' : 'col col-4 col-sm-10 offset-col-sm-1 videocontainer'}, {},Div[i][j], Info[i][j]);
     Videos[i][j].onclick = function() {
       document.location.href = `video.html?id=${videoId}`;
     };
-    VideoRows[i].appendChild(Videos[i][j]);
+    VideoRows[i].appendChild(Videos[i][j])
   }
   Flokkur[i] = element('div', {'class' : 'grid'}, {}, Headers[i], VideoRows[i]);
   main.appendChild(Flokkur[i]);
+
 }
 
 }
